2ba41b6c0040244614ce716d4ae8df167f719604
FARAZ\fabiy <fabiyabani@gmail.com>
2022-03-05 09:47:22 +0530

    commit

---
+++ M11/src/Demo/DataType/DataType.java
@@ -0,0 +1,13 @@
+package Demo.DataType;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		
+		byte a=128;//type not matching.
+		short b=17.2;//type not matching.
+		System.out.println(a);
+		
+	}
+
+}
