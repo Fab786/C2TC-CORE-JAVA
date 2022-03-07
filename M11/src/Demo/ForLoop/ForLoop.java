060922757a26a55bd6e5055cbc493823cdc893f4
FARAZ\fabiy <fabiyabani@gmail.com>
2022-03-07 11:16:34 +0530

    commit

---
+++ M11/src/Demo/ForLoop/ForLoop.java
@@ -0,0 +1,19 @@
+package Demo.ForLoop;
+
+public class ForLoop {
+@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i = 0; i < 0; i++)
+	;
+	for(int i = 0; i < 10; i++)
+System.out.println("Hello Batch 11");
+
+System.out.println("*******************");
+	
+int i = 0;
+
+for(;;);
+}
+
+}
