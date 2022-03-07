0c6b6a6516838d1d3f847217c0b52e9d5b3196b4
FARAZ\fabiy <fabiyabani@gmail.com>
2022-03-07 11:29:26 +0530

    commit

---
+++ M11/src/ArrayDemo/ArrayDemo.java
@@ -0,0 +1,13 @@
+package ArrayDemo;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    byte[][] arr = new byte[2][3];
+    System.out.println(arr);
+    System.out.println(arr[0]);
+    System.out.println(arr[0][0]);
+	}
+
+}
