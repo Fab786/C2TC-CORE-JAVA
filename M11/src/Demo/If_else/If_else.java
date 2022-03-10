bcd3e013439f93b20dcf97862c2ca1b235a69207
FARAZ\fabiy <fabiyabani@gmail.com>
2022-03-10 08:27:15 +0530

    Commit

---
+++ M11/src/Demo/If_else/If_else.java
@@ -0,0 +1,22 @@
+package Demo.If_else;
+
+public class If_else {
+
+	public static void main(String[] args) {
+		
+		boolean b = true;
+		if(b=false) {
+			
+			System.out.println("Good morning");
+		}
+	
+		else
+		{
+			System.out.println("Good noon");
+			
+		}
+	}
+	
+	
+
+}
