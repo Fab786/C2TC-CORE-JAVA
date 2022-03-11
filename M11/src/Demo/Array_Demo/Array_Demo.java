9312bd1569bd34743b45a17cbff8d1cda684ad05
FARAZ\fabiy <fabiyabani@gmail.com>
2022-03-11 15:29:30 +0530

    Commit

---
+++ M11/src/Demo/Array_Demo/Array_Demo.java
@@ -0,0 +1,15 @@
+package Demo.Array_Demo;
+
+public class Array_Demo {
+
+	public static void main(String[] args) {
+
+
+
+		int[] arr=new int[3];
+	System.out.println(arr.getClass().getName()); //object class is used
+	
+	
+	}
+
+}
